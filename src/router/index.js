340969389171ae3052d1efa53df40f7a218c2e54
Oshan Shrestha <os02411418@student.ku.edu.np>
2020-11-23 09:27:59 +0545

    custom routing folder removed

--- src/router/index.js
+++
@@ -1,7 +0,0 @@
-export const createRouter = (Routes, Router) => {
-  let routes = {};
-  Object.keys(Routes).forEach((item) => {
-    routes[item] = item;
-  });
-  return { routes, navigate: Router };
-};
